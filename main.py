a752454172bb3e29d664f016cdc76cce5817a75e
Colevy Collins <77639915+Colevy-Collins@users.noreply.github.com>
2023-11-13 09:36:47 +0000

    extraced user input validation and send search

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 import sqlite3
 import re
 
+def verify_input(user_input, data_type):
+    if data_type == "date":
+        if not verify_date(user_input):
+            print("Invalid date format. Please use the format YYYY-MM-DD.")
+            return False
+
+    elif data_type == "time":
+        if not verify_time(user_input):
+            print("Invalid time format. Please use the format HH:MM.")
+            return False
+    
+    elif data_type == "option":
+        if user_input not in ["1", "2"]:
+            print("Invalid option. Please enter 1 or 2.")
+            return False
+
+    elif data_type == "search_option":
+        if user_input not in [*map(str, range(1, 7))]:
+            print("Invalid option. Please enter 1 or 2.")
+            return False
+
+    else:
+        print("Invalid input. Please try again.")
+        return False
+
+    return True
+
 # Function to verify date format
 def verify_date(date):
-    if re.match(r'^\d{4}-\d{2}-\d{2}$', date):
-        return True
-    return False
+    return re.match(r'^\d{4}-\d{2}-\d{2}$', date)
+
 # Function to verify time format
 def verify_time(time):
-    if re.match(r'^([01]\d|2[0-3]):([0-5]\d)$', time):
-        return True
-    return False
+    return re.match(r'^([01]\d|2[0-3]):([0-5]\d)$', time)
+
+def search_and_print_results(cursor):
+    # Fetch and print the search results
+    search_results = cursor.fetchall()
+
+    if search_results:
+        print("Search Results:")
+        for result in search_results:
+            print("ID:", result[0])
+            print("Date of Task:", result[1])
+            print("Start Time of Task:", result[2])
+            print("End Time of Task:", result[3])
+            print("Task Name:", result[4])
+            print("Task Tag:", result[5])
+            print("------------------------------")
+    else:
+        print("No results found for the search criteria.")
 
 def main():
-
     # Create a new database or connect to an existing one
     connection = sqlite3.connect('tasks.db')
 
@@ -32,23 +72,26 @@ def main():
     print("What would you like to do.")
     print("Enter 1 to input information")
     print("Enter 2 to search for information")
-    task_to_do = input("What do you want to, Enter your choice 1 - 2:")
+    
+    while True:
+        task_to_do = input("What do you want to, Enter your choice 1 - 2:")
+        if verify_input(task_to_do, "option"):
+            break
+
     # Get user input and verify
-    if(task_to_do == "1"):
+    if task_to_do == "1":
         date_of_task = input("Enter date (YYYY-MM-DD): ")
-        while not verify_date(date_of_task):
-            print("Invalid date format. Please use the format YYYY-MM-DD.")
+        while not verify_input(date_of_task, "date"):
             date_of_task = input("Enter date (YYYY-MM-DD): ")
 
         start_time_of_task = input("Enter start time in military time / 24 base (HH:MM): ")
-        while not verify_time(start_time_of_task):
-            print("Invalid time format. Please use the format HH:MM.")
+        while not verify_input(start_time_of_task, "time"):
             start_time_of_task = input("Enter start time in military time / 24 base (HH:MM): ")
 
         end_time_of_task = input("Enter end time in military time / 24 base (HH:MM): ")
-        while not verify_time(end_time_of_task):
-            print("Invalid time format. Please use the format HH:MM.")
+        while not verify_input(end_time_of_task, "time"):
             end_time_of_task = input("Enter end time in military time / 24 base (HH:MM): ")
+
         task_name = input("Enter task name: ")
         task_tag = input("Enter task tag: ")
 
@@ -61,8 +104,7 @@ def main():
 
         print("Data successfully inserted into the database.")
 
-        connection.close()
-    elif(task_to_do == "2"):
+    elif task_to_do == "2":
         cursor = ""
 
         print("What would you like to do.")
@@ -72,12 +114,16 @@ def main():
         print("Enter 4 to search for tasks with a certain name")
         print("Enter 5 to search for tasks with a certain tag")
         print("Enter 6 to search for tasks with a certain start and end time (in military time / 24 base)")
-        task_to_do = input("What do you want to do, Enter your choice 1 - 6:")
+        
+        while True:
+            task_to_do = input("What do you want to do, Enter your choice 1 - 6:")
+            if verify_input(task_to_do, "search_option"):
+                break
 
-        if(task_to_do == "1"):
+        if task_to_do == "1":
             # Search the database based on user input
             search_date = input("Enter the date you want to search for (YYYY-MM-DD): ")
-            while not verify_date(search_date):
+            while not verify_input(search_date, "date"):
                 print("Invalid date format. Please use the format YYYY-MM-DD.")
                 search_date = input("Enter the date you want to search for (YYYY-MM-DD): ")
             
@@ -85,9 +131,11 @@ def main():
             cursor = connection.execute('''
             SELECT * FROM tasks WHERE date_of_task = ?
             ''', (search_date,))
-        if(task_to_do == "2"):
+            search_and_print_results(cursor)
+        
+        elif task_to_do == "2":
             search_start_time = input("Enter the start time you want to search for (HH:MM): ")
-            while not verify_time(search_start_time):
+            while not verify_input(search_start_time, "time"):
                 print("Invalid time format. Please use the format HH:MM.")
                 search_start_time = input("Enter the start time you want to search for (HH:MM): ")
 
@@ -95,10 +143,11 @@ def main():
             cursor = connection.execute('''
             SELECT * FROM tasks WHERE start_time_of_task = ?
             ''', (search_start_time,))
+            search_and_print_results(cursor)
 
-        if(task_to_do == "3"):
+        elif task_to_do == "3":
             search_end_time = input("Enter the end time you want to search for (HH:MM): ")
-            while not verify_time(search_end_time):
+            while not verify_input(search_end_time, "time"):
                 print("Invalid time format. Please use the format HH:MM.")
                 search_end_time = input("Enter the end time you want to search for (HH:MM): ")
 
@@ -106,28 +155,34 @@ def main():
             cursor = connection.execute('''
             SELECT * FROM tasks WHERE end_time_of_task = ?
             ''', (search_end_time,))
-        if(task_to_do == "4"):
+            search_and_print_results(cursor)
+
+        elif task_to_do == "4":
             search_task_name = input("Enter the task name you want to search for: ")
 
             # Query the database based on the user input
             cursor = connection.execute('''
             SELECT * FROM tasks WHERE task_name = ?
             ''', (search_task_name,))
-        if(task_to_do == "5"):
+            search_and_print_results(cursor)
+
+        elif task_to_do == "5":
             search_task_tag = input("Enter the task tag you want to search for: ")
 
             # Query the database based on the user input
             cursor = connection.execute('''
             SELECT * FROM tasks WHERE task_tag = ?
             ''', (search_task_tag,))
-        if(task_to_do == "6"):
+            search_and_print_results(cursor)
+
+        elif task_to_do == "6":
             search_start_time = input("Enter the start time you want to search for (HH:MM): ")
-            while not verify_time(search_start_time):
+            while not verify_input(search_start_time, "time"):
                 print("Invalid time format. Please use the format HH:MM.")
                 search_start_time = input("Enter the start time you want to search for (HH:MM): ")
 
             search_end_time = input("Enter the end time you want to search for (HH:MM): ")
-            while not verify_time(search_end_time):
+            while not verify_input(search_end_time, "time"):
                 print("Invalid time format. Please use the format HH:MM.")
                 search_end_time = input("Enter the end time you want to search for (HH:MM): ")
 
@@ -135,27 +190,9 @@ def main():
             cursor = connection.execute('''
             SELECT * FROM tasks WHERE start_time_of_task = ? AND end_time_of_task = ?
             ''', (search_start_time, search_end_time))
+            search_and_print_results(cursor)
 
-
-        # Fetch and print the search results
-        search_results = cursor.fetchall()
-
-        if search_results:
-            print("Search Results:")
-            for result in search_results:
-                print("ID:", result[0])
-                print("Date of Task:", result[1])
-                print("Start Time of Task:", result[2])
-                print("End Time of Task:", result[3])
-                print("Task Name:", result[4])
-                print("Task Tag:", result[5])
-                print("------------------------------")
-        else:
-            print("No results found for the search criteria.")
-
-        connection.close()
-    else:
-        print("invalid input, please try again")
+    connection.close()
 
 if __name__ == "__main__":
     main()
